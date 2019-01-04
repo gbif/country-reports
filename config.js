f1c9514bcc060a93586c5a06e7d21c209231926e
Matthew Blissett <mblissett@gbif.org>
2019-01-04 15:34:15 +0100

    Default to UAT.

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
 module.exports = {
-    ANALYTICS_COUNTRY_BASEURL: 'https://www.gbif.org/sites/default/files/gbif_analytics/country/',
-    ANALYTICS_GLOBAL_BASEURL: 'https://www.gbif.org/sites/default/files/gbif_analytics/global/',
+    ANALYTICS_COUNTRY_BASEURL: 'https://www.gbif-uat.org/sites/default/files/gbif_analytics/country/',
+    ANALYTICS_GLOBAL_BASEURL: 'https://www.gbif-uat.org/sites/default/files/gbif_analytics/global/',
+
     API_BASE_URL: 'http://api.gbif.org/v1/',
-    API_BASE_URL_STATS: 'http://mb.gbif.org:8080/',
+    API_BASE_URL_STATS: 'http://api.gbif.org/v1/',
+
     HIGHCHARTS_SERVER_URL: 'http://127.0.0.1:7801', // 'http://export.highcharts.com/'
     CONTENTFUL_SEARCH_URL: 'http://cms-search.gbif.org:9200/'
 };
